4f7273ea11471867cc760d453951e242cd3c7cf1
Mr-Shahadat321 <sahadat8278@gmail.com>
2026-01-15 10:04:27 +0800

    practices js dataTypes

---
+++ 01_Variables.js
@@ -0,0 +1,9 @@
+const myId = 12345;
+let myPassword = "Hossain54321";
+var myBirthPlace = "Noakhali";
+myPassword = "87654321"
+// myId = 231;
+
+mypassword = "12345";
+
+console.table([myId, myPassword, myBirthPlace, myPassword, mypassword]);
---
+++ 02_dataTypes.js
@@ -0,0 +1,17 @@
+//js data types
+
+//1) number.
+//2) bignit/intiger.
+//3) string.
+//4) boolean => true/ false.
+//5) null.
+//6) undefined.
+
+
+let abdeshAge = 32;
+
+
+console.log(typeof abdeshAge);
+console.log(typeof null);
+console.log(typeof undefined);
+
